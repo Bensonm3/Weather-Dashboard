4e065f173b4d4c352da60c93f66cb9945f731c71
Matt Benson <rabenson6@msn.com>
2020-01-20 11:23:49 -0800

    working city search

---
+++ test/array.js
@@ -0,0 +1,46 @@
+let array = {
+    "coord": {
+    "lon": -122.03,
+    "lat": 47.53
+    },
+    "weather": [
+    {
+    "id": 500,
+    "main": "Rain",
+    "description": "light rain",
+    "icon": "10d"
+    }
+    ],
+    "base": "stations",
+    "main": {
+    "temp": 282.71,
+    "feels_like": 280.07,
+    "temp_min": 279.82,
+    "temp_max": 285.37,
+    "pressure": 1022,
+    "humidity": 71
+    },
+    "visibility": 16093,
+    "wind": {
+    "speed": 2.04,
+    "deg": 114
+    },
+    "rain": {
+    "1h": 0.25
+    },
+    "clouds": {
+    "all": 90
+    },
+    "dt": 1579470206,
+    "sys": {
+    "type": 1,
+    "id": 3417,
+    "country": "US",
+    "sunrise": 1579448916,
+    "sunset": 1579481311
+    },
+    "timezone": -28800,
+    "id": 5798487,
+    "name": "Issaquah",
+    "cod": 200
+    }
